92b5249657c9670312c4accffa5777e9c27dd8a6
Tim Anderegg <timothy.anderegg@gmail.com>
2025-08-14 16:41:53 -0400

    Adds pop up with plant info, and filter option.

--- app/src/components/map/map.tsx
+++ app/src/components/map/map.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
-import { useState, useEffect } from "react";
+import React, {useState, useEffect, useCallback, useRef} from "react";
 
-import {DeckGL} from "@deck.gl/react";
-import {MapViewState} from "@deck.gl/core";
-import {PMTilesSource, PMTilesMetadata} from '@loaders.gl/pmtiles';
+//import {DeckGL} from "@deck.gl/react";
+
+import {MapViewState, DeckProps} from "@deck.gl/core";
+import {MapboxOverlay} from "@deck.gl/mapbox";
+import {createDataSource} from '@loaders.gl/core';
+import {PMTilesSource} from '@loaders.gl/pmtiles';
 
 import {GeoJsonLayer} from '@deck.gl/layers';
 
-import Map from "react-map-gl";
-import maplibregl, { StyleSpecification } from 'maplibre-gl';
+import {
+  Map,
+  Source,
+  Layer,
+  useControl,
+  NavigationControl,
+  FullscreenControl,
+  ScaleControl,
+  GeolocateControl,
+  Popup
+} from "react-map-gl/maplibre";
+import type {CircleLayer} from 'react-map-gl/maplibre';
 import 'maplibre-gl/dist/maplibre-gl.css';
+import maplibregl from 'maplibre-gl';
 
 import {TileSourceLayer} from '@/layers/tile-source-layer'
 
@@ -22,16 +35,117 @@ import {TileSourceLayer} from '@/layers/tile-source-layer'
   console.log("Successful RW API Call.")
   return await response.json();
 }*/
+const style = {
+  version: 8,
+  sources: {
+    osm: {
+      type: "raster",
+      tiles: ["https://tile.openstreetmap.org/{z}/{x}/{y}.png"],
+      tileSize: 256,
+      attribution: "&copy; OpenStreetMap Contributors",
+      maxzoom: 19
+    }
+  },
+  layers: [
+    {
+      id: "osm",
+      type: "raster",
+      source: "osm" // This must match the source key above
+    }
+  ]
+};
+
+const powerwatchStyle: CircleLayer = {
+  id: "powerwatch-layer",
+  type: "circle",
+  source: "powerwatch",
+  "source-layer": "default",
+  paint: {
+    "circle-radius": [
+      "interpolate",
+      ['linear'],
+      ["zoom"],
+      0,
+      [
+        "+",
+        0.1,
+        [
+          "/",
+          [
+            "sqrt",
+            ["/", ["get", "capacity_mw"], ["pi"]]
+          ],
+          20
+        ]
+      ],
+      12,
+      [
+        "+",
+        4,
+        [
+          "/",
+          [
+            "sqrt",
+            ["/", ["get", "capacity_mw"], ["pi"]]
+          ],
+          3
+        ]
+      ]
+    ],
+    "circle-color": [
+      "match",
+      ["get", "primary_fuel"],
+      "Gas", "rgb(188, 128, 189)",
+      "Oil", "rgb(177, 89, 40)",
+      "Hydro", "rgb(31, 120, 180)",
+      "Nuclear", "rgb(227, 26, 28)",
+      "Solar", "rgb(255, 127, 0)",
+      "Waste", "rgb(106, 61, 154)",
+      "wind", "rgb(92, 162, 209)",
+      "Geothermal", "rgb(253, 191, 111)",
+      "Biomass", "rgb(34, 154, 0)",
+      "rgb(178, 223, 138)"
+    ],
+    "circle-opacity": 0.7
+  }
+}
+
+const cql2Query = `country = 'FRA'`
+const data = [
+  {sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781]}
+];
 
 const WRIMap = () => {
-  const [viewState, setViewState] = useState<MapViewState>({
+  const mapRef = useRef(null);
+  const [plantPopupInfo, setPlantPopupInfo] = useState(null);
+  const [layers, setLayers] = useState([]);
+  const [cursor, setCursor] = useState<string>('auto');
+
+  /*const [viewState, setViewState] = useState<MapViewState>({
     longitude: -1.7138671,
     latitude: 42.0003167,
-    zoom: 4,
-    pitch: 0,
-    bearing: 0
-  });
+    zoom: 4
+  });*/
 
+  const mapClick = useCallback((e) => {
+    const feature = e.features && e.features[0];
+    if (feature) {
+      setPlantPopupInfo({
+        longitude: e.lngLat.lng,
+        latitude: e.lngLat.lat,
+        ...e.features[0].properties
+      })
+    };
+  }, []);
+
+  const mouseEnter = useCallback(() => setCursor('pointer'), []);
+  const mouseLeave = useCallback(() => setCursor('auto'), []);
+  
+  function DeckGLOverlay(props: DeckProps) {
+    const overlay = useControl<MapboxOverlay>(() => new MapboxOverlay(props));
+    overlay.setProps(props);
+    return null;
+  }
   //let [tclMetadata, setTclMetadata] = useState(null);
   //let metadata = null;
 
@@ -47,49 +161,34 @@ const WRIMap = () => {
     getLineWidth: 1
   });*/
 
-  let [layers, setLayers] = useState([]);
-
-  const style = {
-    version: 8,
-    sources: {
-      osm: {
-        type: "raster",
-        tiles: ["https://tile.openstreetmap.org/{z}/{x}/{y}.png"],
-        tileSize: 256,
-        attribution: "&copy; OpenStreetMap Contributors",
-        maxzoom: 19
-      }
-    },
-    layers: [
-      {
-        id: "osm",
-        type: "raster",
-        source: "osm" // This must match the source key above
-      }
-    ]
-  };
-  
-  const data = [
-    {sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781]}
-  ];
-
-  let euroCropLayerSource = new PMTilesSource({
+  /*let euroCropLayerSource = new PMTilesSource({
     url: 'https://s3.us-west-2.amazonaws.com/us-west-2.opendata.source.coop/cholmes/eurocrops/eurocrops-all.pmtiles',
     attributions: ['https://beta.source.coop/repositories/cholmes/eurocrops/description/'],
     loadOptions: {tilejson: {maxValues: 10}}
-  });
+  });*/
+
+
+  // Might be a bug in loaders.gl, doesn't recognize type properly: https://github.com/visgl/loaders.gl/blob/v4.3.4/modules/core/src/lib/api/create-data-source.ts#L20
+  /*const euroCropLayerSource = createDataSource(
+    'https://s3.us-west-2.amazonaws.com/us-west-2.opendata.source.coop/cholmes/eurocrops/eurocrops-all.pmtiles',
+    [PMTilesSource],
+    {
+      attributions: ['https://beta.source.coop/repositories/cholmes/eurocrops/description/'],
+      loadOptions: {tilejson: {maxValues: 10}}
+    }
+  );*/
 
   //console.log("PMTileSource props: " + JSON.stringify(euroCropLayerSource.props, null, 2));
 
-  const euroCropLayer = new TileSourceLayer({
+  /*const euroCropLayer = new TileSourceLayer({
     id: 'EuroCropLayer',
     tileSource: euroCropLayerSource
-  });
+  });*/
 
-  useEffect(() => {
+  /*useEffect(() => {
     const updateLayers = async () => {
       //tclLayer = await getTCLLayer()
-      setLayers([euroCropLayer]);
+      setLayers([]);
       //setLayers([tclLayer, euroCropLayer]);
     }
 
@@ -100,25 +199,74 @@ const WRIMap = () => {
     displayEuroCropsLayer().catch(console.error);
     updateLayers().catch(console.error);
   }, [viewState]);
+  */
 
-  const onViewStateChange = ({ viewState }) => {
+  /*const onViewStateChange = ({ viewState }) => {
     setViewState(viewState);
-  }
+  }*/
 
   return (
-    <DeckGL 
-      viewState={viewState}
-      onViewStateChange={onViewStateChange}
-      controller={true}
-      layers={layers}
+    <Map
+      initialViewState={{
+        longitude: -1.7138671,
+        latitude: 42.0003167,
+        zoom: 4
+      }}
+      mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+      cursor={cursor}
+      onClick={mapClick}
+      onMouseEnter={mouseEnter}
+      onMouseLeave={mouseLeave}
+      interactiveLayerIds={['powerwatch-layer']}
     >
-      <Map
-        mapLib={maplibregl}
-        mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
-      />
-    </DeckGL>
+      <GeolocateControl position="top-left" />
+      <FullscreenControl position="top-left" />
+      <NavigationControl position="top-left" />
+      <ScaleControl />
+
+      {plantPopupInfo && (
+        <Popup
+          anchor="top"
+          longitude={Number(plantPopupInfo.longitude)}
+          latitude={Number(plantPopupInfo.latitude)}
+          onClose={() => setPlantPopupInfo(null)}
+        >
+          <div>
+            <h2>Country</h2>
+            {plantPopupInfo.country}
+            <h2>Name</h2>
+            {plantPopupInfo.name}
+            <h2>Fuel Type</h2>
+            {plantPopupInfo.primary_fuel}
+            <h2>Owner</h2>
+            {plantPopupInfo.owner || '-'}
+            <h2>Capacity</h2>
+            {plantPopupInfo.capacity_mw}
+            <h2>Data Source</h2>
+            {plantPopupInfo.source}
+            <h2>Reported Generation (2018)</h2>
+            {plantPopupInfo.generation_gwh_2018 || 'Unknown'}
+            <h2>Estimated Generation (2017)</h2>
+            {plantPopupInfo.estimated_generation_gwh_2017 || 'Unknown'}
+          </div>
+        </Popup>
+      )}
+
+      <Source
+        id="powerwatch"
+        type="vector"
+        tiles={["https://eoapi.datalab.foo/vector/collections/public.powerwatch_data_20180102/tiles/WebMercatorQuad/{z}/{x}/{y}?filter="+encodeURIComponent(cql2Query)]}
+        minzoom={0}
+        maxzoom={14}
+      >
+        <Layer {...powerwatchStyle} />
+      </Source>
+    </Map>
   );
 }
+
+// Currently prevents cursor change for some reason?
+// <DeckGLOverlay layers={layers} interleaved="false" />
 
 export {
   WRIMap,
